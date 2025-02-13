0a28c663a7b3f6b0dc26febc4e83045116efbb37
gabriel <fabrogabrielandres@gmail.com>
2025-02-13 11:08:40 +0100

    ..

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -14,7 +14,8 @@ export async function POST(req, res) {
       subject: subject,
       react: (
         <>
-          <h1>Email {fromEmail}</h1>
+          <h1>fromEmail {fromEmail}</h1>
+          <h1>email {email}</h1>
           <h1>{subject}</h1>
           <p>Thank you for contacting us!</p>
           <p>New message submitted:</p>
